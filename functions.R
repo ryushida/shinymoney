14502f7a64c9a529093a760d82d49c4f813c9f0f
Ryuhei Shida <ryushidadev@gmail.com>
2021-01-16 18:41:51 -0800

    Make create_heatmap function

--- functions.R
+++ functions.R
@@ -3,3 +3,12 @@ get_id <- function(con, query, value, colname) {
   dbBind(id, c(value))
   id <- dbFetch(id)[[colname]]
 }
+
+
+create_heatmap <- function(df) {
+  ggplot(df, aes(week_of_month, -day_of_week, fill = sum)) +
+    geom_tile() +
+    facet_grid(df$year~df$month) +
+    scale_fill_gradient(low = "#99FF99", high = "#006600") +
+    labs(x = "Week of Months", y = "Day of Weeks")
+}
--- server.R
+++ server.R
@@ -135,11 +135,7 @@ function(input, output, session) {
   })
 
   output$expense_amount_heatmap <- renderPlot({
-    ggplot(expense_amounts(), aes(week_of_month, -day_of_week, fill = sum)) +
-      geom_tile() +
-      facet_grid(expense_amounts()$year~expense_amounts()$month) +
-      scale_fill_gradient(low = "#99FF99", high = "#006600") +
-      labs(x = "Week of Months", y = "Day of Weeks")
+    create_heatmap(expense_amounts())
   })
   
   output$expense_amount <- renderPlot({
