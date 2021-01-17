a584a35267ad4834531f414f745646814bf1fe24
Ryuhei Shida <ryushidadev@gmail.com>
2021-01-16 19:10:44 -0800

    Add Day of Week Text

--- functions.R
+++ functions.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 get_id <- function(con, query, value, colname) {
   id <- dbSendQuery(con, query)
   dbBind(id, c(value))
@@ -6,9 +8,23 @@ get_id <- function(con, query, value, colname) {
 
 
 create_heatmap <- function(df) {
-  ggplot(df, aes(week_of_month, -day_of_week, fill = sum)) +
+  ggplot(add_day_text(df), aes(week_of_month, day_of_week_text, fill = sum)) +
     geom_tile() +
     facet_grid(df$year~df$month) +
     scale_fill_gradient(low = "#99FF99", high = "#006600") +
     labs(x = "Week of Months", y = "Day of Weeks")
+}
+
+add_day_text <- function(df) {
+  df2 <- df %>% mutate(
+    day_of_week_text = case_when (
+      day_of_week == 0 ~ "7:Sunday",
+      day_of_week == 1 ~ "6:Monday",
+      day_of_week == 2 ~ "5:Tuesday",
+      day_of_week == 3 ~ "4:Wednesday",
+      day_of_week == 4 ~ "3:Thursday",
+      day_of_week == 5 ~ "2:Friday",
+      day_of_week == 6 ~ "1:Saturday",
+    )
+  )
 }
